4b81f043d3e9a971b7b74457aa0a40f6a942888f
daidaidal <15636811081@163.com>
2017-03-26 18:29:44 +0800

    递归first

--- parser.cpp
+++ parser.cpp
@@ -6,13 +6,16 @@
 #define BUFFERSIZE 10000
 
 using namespace std;
-multimap<string, node*> a;
-typedef multimap<string, node*>::iterator it;
+multimap<string, node*> a; //最开始的状态集
+map<string, set<string>> first_set; //first集合
+typedef multimap<string, node*>::iterator sn_it;
+typedef map<string, set<string>>::iterator ss_it;
+typedef set<string>::iterator set_it;
 char *buffer = new char[BUFFERSIZE];
 int colon_judge = 0;
 int buffer_count = 0;
 int state_count = 0;
-map<string, node*> state_map[100];
+map<string, node*> state_map[100]; //有可能不是拷贝而是引用地址，需验证
 node *start = new node("", "");
 node *p = NULL;
 //标准化处理
@@ -60,6 +63,8 @@ int s()
 		start->next = new node(s, p);
 		start = start->next;
 		a.insert(pair<string, node*>(s, start));
+		set<string> y;
+		first_set.insert(pair<string,set<string>>(s, y)); //有可能连接着同一个y
 	}
 	return 1;
 }
@@ -69,7 +74,7 @@ void standardize()
 	p = start;
 	ifstream in;
 	string filename;
-	in.open("222.txt");
+	in.open("temp.txt");
 	if (!in)
 	{
 		cout << "open error" << endl;
@@ -87,7 +92,61 @@ void standardize()
 	while (s());
 	return;
 }
-void first(string n){}
+/*
+set<string> first(string beita, set<string>aerfa, set<string>*last)
+{
+
+}
+*/
+//递归版本
+set<string> make_first(string s)
+{
+	//终结符号表
+	//如果找不到别的以他为首字母的就是终结符
+	//first=本身就是终结符
+	//递归函数
+	ss_it f_s = first_set.find(s);
+	if (f_s == first_set.end())
+	{
+		set<string> ret;
+		ret.insert(s);
+		return ret;
+	}
+	pair<sn_it, sn_it> f = a.equal_range(s);
+	for (sn_it k = f.first; k != f.second; k++)
+	{
+		//把每个的第一个加进去,如果可能出现空的话就把后面的加进去 while(1) if 通过string 剔除第一个 第二个 。。第 n个
+		string first = k->second->p.substr(k->second->p.find("@") + 1, k->second->p.find(" ", k->second->p.find("@"))-1);
+		set<string> temp = make_first(first);
+		for (set_it m = temp.begin(); m != temp.end(); m++)
+			f_s->second.insert(*m);
+	}
+	return f_s->second;
+}
+//非递归版本
+void make_firstset()
+{
+	string start_string = "postfix_expression";
+	string s;
+	ss_it f_s = first_set.find(s);
+	if (f_s == first_set.end())
+	{
+		set<string> ret;
+		ret.insert(s);
+		return ret;
+	}
+	pair<sn_it, sn_it> f = a.equal_range(s);
+	for (sn_it k = f.first; k != f.second; k++)
+	{
+		//把每个的第一个加进去,如果可能出现空的话就把后面的加进去 while(1) if 通过string 剔除第一个 第二个 。。第 n个
+		string first = k->second->p.substr(k->second->p.find("@") + 1, k->second->p.find(" ", k->second->p.find("@")) - 1);
+		set<string> temp = make_first(first);
+		for (set_it m = temp.begin(); m != temp.end(); m++)
+			f_s->second.insert(*m);
+	}
+	return f_s->second;
+}
+/*
 void closure(node* n)
 {
 	map<string, node*> temp_map;
@@ -103,40 +162,52 @@ void closure(node* n)
 		temp_map.insert(pair<string, node*>(n->s, n));
 		stackl.pop();
 		string first = n->p.substr(n->p.find("@") + 1, n->p.find(" ", n->p.find("@")));
-
+		string second;
 		if (n->p.find("", n->p.find("@")) == -1)
-		{
 			insert_symbol = n->symbol;
-		}
 		else
-			insert_symbol = first(second, a);
+		{
+			second = n->p.substr(n->p.find(" ", n->p.find("@")) + 1, n->p.find(" ", n->p.find(" ", n->p.find("@"))));
+			first(second, n->symbol, &insert->symbol);
+		}
 
 		if (first == "")
 			continue;
 		if (setl.find(first) != setl.end())
 		{
-			pair<it, it> ret = a.equal_range(first);
+			pair<sn_it, sn_it> ret = a.equal_range(first);
 			setl.insert(first);
-			for (it k = ret.first; k != ret.second; k++)
+			for (sn_it k = ret.first; k != ret.second; k++)
 			{
 				//J=J∪{[B→.η,b]|[A→α.Bβ,a]∈J, b∈FIRST(βa)}	
 				node * temp_n = k->second;
 				temp_n->symbol.//把insert_symbol和temp_n->symbol合并即可
-				stackl.push(k->second);
+					stackl.push(k->second);
 			}
 		}
 	}
-	
+
 }
+*/
 
 void main()
 {
 	standardize();
+	make_first("postfix_expression");
+	
 	node *c = p->next;
 	while (c != NULL)
 	{
 		cout << c->s << ":" << c->p << endl;
 		c = c->next;
+	}
+	cout << endl << "first:" << endl;
+	for (ss_it itt = first_set.begin(); itt != first_set.end(); itt++)
+	{
+		cout << endl<<itt->first << ":";
+		set<string>::iterator si;
+		for (si = itt->second.begin();si != itt->second.end();si++)
+			cout << *si << " ";
 	}
 	/*
 	pair<it, it> ret = n->p.equal_range("primary_expression");
